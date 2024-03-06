1372184f4aa8580cebd86a6cf4e0487433a825ca
Tobias Rusås Olsen <tobias.olsen@spv.no>
2024-03-06 13:28:06 +0100

    hm

--- app/plask/tips5-backup.tsx
+++ app/plask/tips5-backup.tsx
@@ -2,16 +2,10 @@ type Routes = Record<string, string | { href: string; protected: boolean }>;
 
 export const advancedRoutes = {
   overview: "/overview",
-  ja: "bla",
-  tjo: 5,
-  sk: {
-    href: "/sk",
-    protected: false,
-  },
   admin: {
     href: "/admin",
     protected: true,
   },
-} as const satisfies Routes;
+};
 
 advancedRoutes.admin.href;
